2ca2419200e3637447b7a8f79944b6069fbde7f3
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-07-17 17:19:43 +0530

    Detailing article structure

--- src/model/article.ts
+++ src/model/article.ts
@@ -5,6 +5,10 @@ const articleSchema = new mongoose.Schema({
   content: String,
   reportAccountPublicKey: String,
   owner: String,
+  date_publish: String,
+  url: String,
+  image_url: String,
+  language: String,
 });
 
 export const Article = mongoose.model("article", articleSchema);
