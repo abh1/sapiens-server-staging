c718564be91300b378d8538bf5978ece4655774a
Abhinav Sharma <abhinavsharma2689@gmail.com>
2022-07-23 00:52:30 +0530

    test

--- src/service/article.ts
+++ src/service/article.ts
@@ -114,7 +114,7 @@ const getArticlesUnderVoting = async (userPublicKey: string) => {
     const articlesList = await Article.aggregate([ 
       {$group:{_id:"$url", ids:{$first:"$_id"}}}]);
 
-      console.log("gogo bears", articlesList.ids);
+      console.log("gogo bears", articlesList[0].ids);
 
 /*     const articlesList = await Article.find({
       _id: unique_articlesList.ids
